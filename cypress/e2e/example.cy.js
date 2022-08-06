b0832deba5b52a8d0775237336c4dff28905bc72
Destiny <dli393@aucklanduni.ac.nz>
2022-08-07 01:00:59 +1200

    Add tiles in a grid

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -5,7 +5,15 @@ describe("the homepage", () => {
     cy.visit("http://localhost:1234");
   });
 
-  it("displays a link to the Capidle game", () => {
-    cy.get("a").should("contain.text", "Click here");
+  it("Capidle link redirects to its web page", () => {
+    cy.get('a[id="capidle-btn"]').should("contain.text", "Capidle");
+    cy.get('a[id="capidle-btn"]').click()
+    cy.url().should('eq', 'http://localhost:1234/capidle/index.html')
+  });
+
+  it("Liedle link redirects to its web page", () => {
+    cy.get('a[id="liedle-btn"]').should("contain.text", "Liedle");
+    cy.get('a[id="liedle-btn"]').click()
+    cy.url().should('eq', 'http://localhost:1234/liedle/index.html')
   });
 });

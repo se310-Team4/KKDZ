96faf3fc5339eee27888125ec04a9543ff3d8035
Destiny <dli393@aucklanduni.ac.nz>
2022-08-09 16:50:06 +1200

    Colour a tile incorrectly 8% of the time
    Considerations:
    - if the full word is correct, none of the tiles will be coloured incorrectly
    - when attempting to colour a tile incorrectly, it will have an equal chance of picking one of the incorrect tile colours
    - lieRate is a global (window) variable to enable determinate testing

--- cypress/e2e/liedle/colour-word.cy.js
+++ cypress/e2e/liedle/colour-word.cy.js
@@ -3,9 +3,10 @@ const word = "stare"
 describe('colour each letter based on if it matches the secret word', () => {
     beforeEach(() => {
         cy.visit("http://localhost:1234/liedle/index.html")
-        // override secret word for testing purposes
+        // override global variables for testing purposes
         cy.window().then((win) => {
             win.secretWord = word
+            win.lieRate = 0.0
         })
         cy.get('body').type('frame').type('{enter}')
     })
@@ -23,5 +24,23 @@ describe('colour each letter based on if it matches the secret word', () => {
     it('test wrong letter wrong place', () => {
         cy.get(`[data-col-index=0][data-row-index=0].tile`).first()
             .should('have.attr', 'data-type', 'wrong')
+    })
+})
+
+describe('game has a chance of incorrectly colouring each letter', () => {
+    beforeEach(() => {
+        cy.visit("http://localhost:1234/liedle/index.html")
+        // override global variables for testing purposes
+        cy.window().then((win) => {
+            win.secretWord = word
+            win.lieRate = 1.0
+        })
+        cy.get('body').type('frame').type('{enter}')
+    })
+
+    it('test lies', () => {
+        cy.get(`[data-col-index=0][data-row-index=0].tile`).first().should('not.have.attr', 'data-type', 'wrong')
+        cy.get(`[data-col-index=1][data-row-index=0].tile`).first().should('not.have.attr', 'data-type', 'right-letter')
+        cy.get(`[data-col-index=2][data-row-index=0].tile`).first().should('not.have.attr', 'data-type', 'right')
     })
 })

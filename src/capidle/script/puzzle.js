5e471445b9cf6e5abb21c3148eec87015f419894
Kyle Hensel <k.y.l.e@outlook.co.nz>
2022-08-11 10:41:20 +1200

    generate database for capidle

--- src/capidle/script/puzzle.js
+++ src/capidle/script/puzzle.js
@@ -1,3 +1,20 @@
-export function generatePuzzle() {
-  return "Tenerife"; // TODO: randomly choose
+// @ts-check
+import capitalCitiesDB from "../puzzles/capitalCities.json";
+import nzCitiesDB from "../puzzles/nzCities.json";
+
+export const gameModes = {
+  capitalCities: {
+    name: "Capital Cities",
+    db: capitalCitiesDB,
+  },
+  nzCities: {
+    name: "New Zealand Towns & Cities",
+    db: nzCitiesDB,
+  },
+};
+
+/** @param {keyof gameModes} gameMode */
+export function generatePuzzle(gameMode) {
+  const { db } = gameModes[gameMode];
+  return db[Math.floor(Math.random() * db.length)];
 }

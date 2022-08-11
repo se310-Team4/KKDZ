ac51b71644834f177d7a63143521fad8d06eba55
Zihan <kanelmis@gmail.com>
2022-08-11 17:13:44 +0800

    Add grid & cell to game board

---
+++ src/2048-expert/2048-expert.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    console.log("this works properly")
+    const gameBoard = document.querySelector('.game-board')
+    const width = 6
+    let cells = []
+
+    // create the game board
+    function createBoard() {
+        for (i = 0; i < width * width; i++) {
+            cell = document.createElement('div')
+            cells.innerHTML = 0
+            gameBoard.appendChild(cell)
+            cells.push(cell)
+        }
+        // intial 2 tiles at random place
+        generateNewTile()
+        generateNewTile()
+    }
+
+    // generate number 2 or 4 at a random cell
+    function generateNewTile() {
+        let rand = Math.floor(Math.random() * cells.length)
+        if (cells[rand].innerHTML == 0) {
+            cells[rand].innerHTML = randomIntFromInterval()
+        } else {// if a cell is already have a number then fine a new tile
+            generateNewTile()
+        }
+    }
+
+    // generate number 2 or 4 randomlly
+    function randomIntFromInterval() {
+        var rand = Math.floor(Math.random())
+        return (rand > 0.5) ? 4 : 2
+    }
+
+
+
+
+
+    createBoard()
+
+})
+

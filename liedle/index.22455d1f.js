8d281795b1c4b51860d10bdcd94088befba74354
Destiny-02 <78716153+Destiny-02@users.noreply.github.com>
2022-08-13 09:24:30 +0000

    Deploying to gh-pages from @ se310-Team4/KKDZ@f6ebed881b038e4aea382b0e9900b713fa157dcf 🚀

---
+++ liedle/index.22455d1f.js
@@ -0,0 +1,2 @@
+const n=document.getElementById("modal"),t=document.getElementById("help-btn");function e(){n.style.display="none"}document.getElementById("close-btn").onclick=function(){e()},t.onclick=function(){n.style.display="block"},window.onclick=function(t){t.target==n&&e()};
+//# sourceMappingURL=index.22455d1f.js.map

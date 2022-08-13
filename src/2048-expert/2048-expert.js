59802caee2e79f7e971ae218d8d124bb16f9086e
Kanelmis <37644658+Kanelmis@users.noreply.github.com>
2022-08-13 16:32:40 +0800

    Implement the grid and tiles closes depends on #35 #36 (#77)
    * Add grid & cell to game board
    
    * Configure the grid and cell
    
    * Add test cases
    
    * fix: makes declarations explict
    
    * fix: replace regex express with character classes
    
    * style: keep all hex value in lowercase
    
    * fix: typo and inappropriate method name fixed
    
    * rename to match convention
    
    * Add class type to cell elements
    
    * Modify test case name to match changes in js

---
+++ src/2048-expert/2048-expert.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', ()=> {
+    const gameBoard = document.querySelector('.game-board')
+    const WIDTH = 6
+    let cells = []
+
+    // create the game board
+    function createBoard() {
+        let totalCell = WIDTH * WIDTH
+        for (let i = 0; i < totalCell; i++) {
+            let cell = document.createElement('div')
+            cell.classList.add("cell")
+            cells.innerHTML = 0
+            gameBoard.appendChild(cell)
+            cells.push(cell)
+        }
+        // intial 2 tiles at random place
+        generateNewTile()
+        generateNewTile()
+    }
+
+    // generate number 2 or 4 at a random cell
+    function generateNewTile() {
+        let rand = Math.floor(Math.random() * cells.length)
+        if (cells[rand].innerHTML == 0) {
+            cells[rand].innerHTML = randomNumTwoOrFour()
+        } else {// if a cell is already have a number then find a new tile
+            generateNewTile()
+        }
+    }
+
+    // generate number 2 or 4 randomlly
+    function randomNumTwoOrFour() {
+        let rand = Math.random()
+        return (rand > 0.5) ? 4 : 2
+    }
+
+    //User actions
+    //Move up
+
+
+
+
+    createBoard()
+
+})
+

6fd3293843b393624f3cdd7272fef01f36da3a56
k-yle <k-yle@users.noreply.github.com>
2022-08-14 12:39:49 +0000

    Deploying to gh-pages from @ se310-Team4/KKDZ@cb72bd553549e3dc10afb40bfbcfe9fa627f6cba 🚀

--- liedle/index.22455d1f.js
+++
@@ -1,2 +0,0 @@
-const n=document.getElementById("modal"),t=document.getElementById("help-btn");function e(){n.style.display="none"}document.getElementById("close-btn").onclick=function(){e()},t.onclick=function(){n.style.display="block"},window.onclick=function(t){t.target==n&&e()};
-//# sourceMappingURL=index.22455d1f.js.map

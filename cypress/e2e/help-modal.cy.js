cb72bd553549e3dc10afb40bfbcfe9fa627f6cba
Kyℓe Hensel <k-yle@users.noreply.github.com>
2022-08-15 00:37:42 +1200

    implement the help modal for Capidle (#85)

---
+++ cypress/e2e/help-modal.cy.js
@@ -0,0 +1,30 @@
+describe("help modal", () => {
+  beforeEach(() => {
+    // we could use any mini game for this test, this is an arbitrary choice
+    cy.visit("http://localhost:1234/liedle");
+  });
+
+  it("has a help window that shows by default", () => {
+    cy.get("[id=modal]").should("have.css", "display", "block");
+  });
+
+  it("can close the help window using the close button", () => {
+    cy.get("[id=close-btn]").click();
+    cy.get("[id=modal]").should("have.css", "display", "none");
+  });
+
+  it("can close the help modal by pressing escape", () => {
+    cy.get("[id=modal]").should("have.css", "display", "block");
+
+    // click the Escape key on the keyboard
+    cy.get("body").type("{esc}");
+
+    cy.get("[id=modal]").should("have.css", "display", "none");
+  });
+
+  it("can reopen the help window", () => {
+    cy.get("[id=close-btn]").click();
+    cy.get("[id=help-btn]").click();
+    cy.get("[id=modal]").should("have.css", "display", "block");
+  });
+});
--- src/modal.js
+++ src/modal.js
@@ -1,12 +1,9 @@
 /** usage: <core-modal>Window contents</core-modal> */
 class Modal extends HTMLElement {
-	constructor() {
-		super();
+  constructor() {
+    super();
 
-		// if you use `<core-modal custom>`, this component does nothing
-		if (this.getAttribute("custom") !== null) return;
-
-		this.innerHTML = `
+    this.innerHTML = `
 			<div>
 				<div id="modal">
 					<div class="modal-window">
@@ -16,7 +13,40 @@ class Modal extends HTMLElement {
 				</div>
 			</div>
 			`;
-	}
+
+    this.registerEventListeners();
+  }
+
+  registerEventListeners() {
+    const modal = document.getElementById("modal");
+    const helpBtn = document.getElementById("help-btn");
+    const closeBtn = document.getElementById("close-btn");
+
+    function onKeyDownInModal(e) {
+      if (e.key === "Escape") closeModal();
+    }
+
+    function closeModal() {
+      modal.style.display = "none";
+      document.removeEventListener("keydown", onKeyDownInModal);
+    }
+
+    function openModal() {
+      modal.style.display = "block";
+      document.addEventListener("keydown", onKeyDownInModal);
+    }
+
+    closeBtn.onclick = closeModal;
+    helpBtn.onclick = openModal;
+
+    // close window if they user clicks outside of the window
+    window.onclick = function (event) {
+      if (event.target == modal) closeModal();
+    };
+
+    // open the window by default
+    openModal();
+  }
 }
 
 window.customElements.define("core-modal", Modal);

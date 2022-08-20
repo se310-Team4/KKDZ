f8b958a60c93a929360953839446803abedaa25a
k1dubv1 <79814736+k1dubv1@users.noreply.github.com>
2022-08-20 14:08:54 +1200

    Home layout upgrade closes #23, #45 (#78)
    * renamed all changeable instances from KKDZ to BrainGain closes #54
    
    * added base application css, font. Implemented initial layout of home screen and added basic animations. Added and passed all test cases.
    
    * fixed not implemented code
    
    Co-authored-by: k1dubv1
    Co-authored-by: Destiny <78716153+Destiny-02@users.noreply.github.com>

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -18,8 +18,47 @@ describe("the homepage", () => {
   });
 
   it("2048 Expert link redirects to its web page", () => {
-    cy.get('a[id="2048expert-btn"]').should("contain.text", "2048 Expert");
-    cy.get('a[id="2048expert-btn"]').click()
+    cy.get('a[id="a2048expert-btn"]').should("contain.text", "2048 Expert");
+    cy.get('a[id="a2048expert-btn"]').click()
     cy.url().should('eq', 'http://localhost:1234/2048-expert/index.html')
+  });
+
+  it("should display a image in all buttons", () => {
+    cy.get('a[id="capidle-btn"]').find("img").should('be.visible');
+    cy.get('a[id="liedle-btn"]').find("img").should('be.visible');
+    cy.get('a[id="a2048expert-btn"]').find("img").should('be.visible');
+  });
+
+  it("should display the title image", () => {
+    cy.get('div[id="center-info"]').find('img[id="title-img"]').should('be.visible');
+  });
+
+  it("should display capidle details when button is hovered", () => {
+    cy.get('a[id="capidle-btn"]').realHover('mouse');
+    cy.get('div[id="center-info"]').find('div[id="capidle-details"]').should('be.visible');
+    cy.wait(1000)
+    cy.get('div[id="capidle-details"]').should("contain.text", "Capidle");
+  });
+
+  it("should display liedle details when button is hovered", () => {
+    cy.get('a[id="liedle-btn"]').realHover('mouse');
+    cy.get('div[id="center-info"]').find('div[id="liedle-details"]').should('be.visible');
+    cy.wait(1000)
+    cy.get('div[id="liedle-details"]').should("contain.text", "Liedle");
+  });
+
+  it("should display 2048 expert details when button is hovered", () => {
+    cy.get('a[id="a2048expert-btn"]').realHover('mouse');
+    cy.get('div[id="center-info"]').find('div[id="a2048expert-details"]').should('be.visible');
+    cy.wait(1000)
+    cy.get('div[id="a2048expert-details"]').should("contain.text", "2048 Expert");
+  });
+
+  it("should redisplay logo when button is no longer hovered", () => {
+    cy.get('a[id="capidle-btn"]').realHover('mouse');
+    cy.wait(1000)
+    cy.get('img[id="title-img"]').realHover('mouse');
+    cy.wait(1000)
+    cy.get('div[id="center-info"]').find('img[id="title-img"]').should('be.visible');
   });
 });

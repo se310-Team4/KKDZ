5b67dccd75ca4f0d0963f0185a2c990529c9c6df
zasheus <ryanmardiono@gmail.com>
2022-09-30 02:18:54 +1300

    fixing test case due to <a> changing to <span>

--- cypress/e2e/homepage.cy.js
+++ cypress/e2e/homepage.cy.js
@@ -11,21 +11,21 @@ describe("the homepage", () => {
   it("links redirect to its web page", () => {
     for (let i = 0; i < miniGames.length; i++) {
       cy.visit("http://localhost:1234");
-      cy.get(`a[id="${miniGames[i]}-btn"]`).should("contain.text", miniGameTitles[i]);
-      cy.get(`a[id="${miniGames[i]}-btn"]`).click();
+      cy.get(`span[id="${miniGames[i]}-btn"]`).should("contain.text", miniGameTitles[i]);
+      cy.get(`span[id="${miniGames[i]}-btn"]`).click();
       cy.url().should("eq", `http://localhost:1234/${miniGameFolders[i]}/index.html`);
     }
   });
 
   it("should display an image in all buttons", () => {
     for (const game of miniGames) {
-      cy.get(`a[id="${game}-btn"]`).find("img").should("be.visible");
+      cy.get(`span[id="${game}-btn"]`).find("img").should("be.visible");
     }
   });
 
   it("should display details when button is hovered", () => {
     for (const game of miniGames) {
-      cy.get(`a[id="${game}-btn"]`).realHover("mouse");
+      cy.get(`span[id="${game}-btn"]`).realHover("mouse");
       cy.get('div[id="center-info"]').find(`div[id="${game}-details"]`).should("be.visible");
     }
   });

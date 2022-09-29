e9809d640a311cae25c48597d9f8c69cccad3632
Zasheus <36759733+Zasheus@users.noreply.github.com>
2022-09-30 11:35:22 +1300

    Animation when clicking game (#131)
    * animation when going to game
    
    * fixing test case due to <a> changing to <span>
    
    * fixed test case
    
    * fixed transition not working due to local issues

--- src/brain-gain/index.js
+++ src/brain-gain/index.js
@@ -20,4 +20,13 @@ function titleShow() {
   document.getElementById("a2048expert-details").style.opacity = "0%";
 }
 
+window.transitionToPage = function (href) {
+  document.querySelector('body').style.opacity = 0
+  setTimeout(function () {
+    window.location.href = href
+  }, 500)
+}
 
+document.addEventListener('DOMContentLoaded', function(event) {
+  document.querySelector('body').style.opacity = 1
+})
